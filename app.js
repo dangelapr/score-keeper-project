7127251283e908956a97216fa28f839a33bac957
D'Angela Pritchett-Rowe <dangela.prit.rowe@gmail.com>
2021-06-29 17:46:09 -0400

    Max score selector logic

--- app.js
+++ app.js
@@ -7,10 +7,32 @@ const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const reset = document.querySelector('#reset');
 
-let winningScore = 5;
+// max score selector
+const winningScoreSelect = document.querySelector('#maxPoints');
+
+// initial variables
+// winningScoreSelect.value = 'placeholder';
+let winningScore = 0;
 let isGameOver = false;
 let p1Score = 0;
 let p2Score = 0;
+
+
+
+// max score selector logic
+winningScoreSelect.addEventListener('change', function() {
+    winningScore = parseInt(this.value);
+    resetGame();
+});
+
+// // buttons disabled until max score is selected
+// if (winningScoreSelect.value === 'placeholder') {
+//     p1Button.disabled = true;
+//     p2Button.disabled = true;
+// } else {
+//     p1Button.disabled = false;
+//     p2Button.disabled = false;
+// }
 
 // player 1 score incrementer
 p1Button.addEventListener('click', function() {
@@ -34,11 +56,17 @@ p2Button.addEventListener('click', function() {
     }
 });
 
-// reset both scores
-reset.addEventListener('click', function() {
-    p1Display.textContent = '0';
-    p2Display.textContent = '0';
+// reset function
+function resetGame() {
+    isGameOver = false;
     p1Score = 0;
     p2Score = 0;
-    isGameOver = false;
-})
+    p1Display.textContent = '0';
+    p2Display.textContent = '0';
+};
+
+// reset both scores and selector
+reset.addEventListener('click', function() {
+    resetGame();
+    winningScoreSelect.value = 'placeholder';
+});
